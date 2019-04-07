3794f19d811e4fe14b55a2457ce6720c8866151b
Luiz Galiza <lfdgaliza@gmail.com>
2019-04-07 11:53:24 +0100

    Login working

---
+++ AspNetCoreGitHubAuth/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreGitHubAuth.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl });
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            SignOut(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ AspNetCoreGitHubAuth/Views/Home/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreGitHubAuth.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public string GitHubAvatar { get; set; }
+
+        public string GitHubLogin { get; set; }
+
+        public string GitHubName { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public void OnGet()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                GitHubName = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                GitHubLogin = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+                GitHubUrl = User.FindFirst(c => c.Type == "urn:github:url")?.Value;
+                GitHubAvatar = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+            }
+        }
+    }
+}
